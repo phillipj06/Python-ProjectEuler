3d0ef745253c2afe249b0e3389dee7f3d3c1f554
Phillip Jensen <phillipj06@gmail.com>
2012-01-20 21:02:45 -0800

    Updated comments in problem3.py

--- problem3.py
+++ problem3.py
@@ -4,6 +4,9 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 '''
 def maxPrimeFactor(number):
+    '''
+    maxPrimeFactor takes a number and returns the largest prime factor for that number
+    '''
     # The largest factor of a number cannot be larger then the square root of the number
     maxFactor = int(number ** 0.5)
     while maxFactor > 1:
@@ -14,8 +17,10 @@ def maxPrimeFactor(number):
     # If the while loop did not return a value then the orginal number has to be prime
     return number
 
-# Function to check if a number is prime
 def primeCheck(num):
+    '''
+    primeCheck checks to see if the number passed to it is a prime number.
+    '''
     if num < 2:
         return False
     elif num == 2:
@@ -29,8 +34,10 @@ def primeCheck(num):
         i += 2
     return True
 
-# A quick test to check the logic for the problem
 def testLogic():
+    '''
+    A quick test to check the logic for the problem
+    '''
     answer =  maxPrimeFactor(13195) 
     if answer != 29: 
         print 'Logic is bad. Answer = 29.  Result =' +str(answer)
