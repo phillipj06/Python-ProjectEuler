32f47465a69eb2fb8f7c6753c030fb3658ad1ead
Phillip Jensen <phillipj06@gmail.com>
2012-01-22 16:15:39 -0800

    Corrected a problem in problem 20

--- problem20.py
+++ problem20.py
@@ -1,8 +1,6 @@
 from math import factorial
 '''
-n! means n x (n - 1) x ... x 3 x 2 x 1 For example,
-10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
-and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+n! means n x (n - 1) x ... x 3 x 2 x 1 For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800, and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 Find the sum of the digits in the number 100!
 
 '''
