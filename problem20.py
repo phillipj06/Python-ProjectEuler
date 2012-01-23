5e36dca6d77cee3f6d22d4276612aa31b2d2c183
Phillip Jensen <phillipj06@gmail.com>
2012-01-22 17:17:27 -0800

    added timer decorator

--- problem20.py
+++ problem20.py
@@ -1,10 +1,11 @@
 from math import factorial
+from timeIt import timeIt
 '''
 n! means n x (n - 1) x ... x 3 x 2 x 1 For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800, and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 Find the sum of the digits in the number 100!
 
 '''
-def sumUp(n):
-    return(sum([int(i) for i in str(n)]))
-
-print sumUp(factorial(100))
+@timeIt
+def sumUp():
+    print (sum([int(i) for i in str(factorial(100))]))
+sumUp()
